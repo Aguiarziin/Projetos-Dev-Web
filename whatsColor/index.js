1d45b07e7a163f5b6f2e170933696084072d6844
Thiago Aguiar <117193209+Th-Aguiar@users.noreply.github.com>
2023-02-21 21:53:58 -0300

    Js e Incremento no Html

---
+++ whatsColor/index.js
@@ -0,0 +1,19 @@
+const btnGenerate = document.getElementById('btn-generate')
+
+function generateColors() {
+    //Numeros hexas decimais vai de 0 a F
+    const chars = '0123456789ABCDEF'
+    //a cor precisa ser armazenada em uma variavel com '#' pro css ler 
+    let color = '#'
+
+    for (let i = 0; i < 6; i++) {
+        //charAt retorna um string do indice passado dessa string
+        color += chars.charAt(Math.floor(Math.random() * (chars.length + 1)))
+        // é passado uma funcao de numeros aleatorios para o CharAt selecionar uma string sempre com cor diferente
+    }
+    return color
+}
+
+btnGenerate.addEventListener('click', () => {
+    // vai ser construida uma função para cada div da paleta receber a cor gerada na função acima
+})
