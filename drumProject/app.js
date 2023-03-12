1617ebf92a53f7c2c16e6944d9a95cf668db351b
Thiago Aguiar <117193209+Th-Aguiar@users.noreply.github.com>
2023-03-12 16:36:52 -0300

    Att Projeto

--- drumProject/app.js
+++ drumProject/app.js
@@ -3,7 +3,10 @@ const buttonDrum = document.querySelectorAll('.drum')
 
 // Adicionar eventos em todos os Buttons
 for (let i = 0; i < buttonDrum.length; i++) {
-    buttonDrum[i].addEventListener('click', () => {
-        alert('Clicou')
-    })
+    buttonDrum[i].addEventListener('click', () => drumAudio())
+}
+
+function drumAudio() {
+    let audio = new Audio('./sounds/tom-1.mp3')
+    audio.play()
 }
